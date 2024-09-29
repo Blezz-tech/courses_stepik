d2cdf3479cee46532629f853721011a9cc87caf1
blezz-tech <markus.jenya04@yandex.ru>
2024-09-29 14:46:28 +0300

    Задача 2 закончена

--- courses/82478/parts/1/tasks/2/answer.py
+++ courses/82478/parts/1/tasks/2/answer.py
@@ -1,23 +1,27 @@
-def tree_hieght(arr, x, height = 0):
-    if x == -1:
-        return height
-    return tree_hieght(arr, arr[x], height + 1)
-    
-
-def work(text):
+def work(text, n):
     arr = list(map(int, text.split(' ')))
+    heights = [-1] * n
 
-    height = 0
-    for x in arr:
-        height = max(height, tree_hieght(arr, x))
+    def compute_height(x):
+        if x == -1:
+            return 0
+        if heights[x] != -1:
+            return heights[x]
+        
+        heights[x] = 1 + compute_height(arr[x])
+        return heights[x]
 
-    return height + 1
+    max_height = 0
+    for x in range(n):
+        max_height = max(max_height, compute_height(x))
+
+    return max_height
 
 # IO side effects
 def main():
-    count = int(input())
+    n = int(input())
     text = input()
-    print(work(text))
+    print(work(text, n))
 
 main()
 
@@ -25,15 +29,24 @@ main()
 
 tasks = [
     {
-        "input": "4 -1 4 1 1",
+        "input": [
+            "4 -1 4 1 1",
+            5
+        ],
         "output": 3,
     },
     {
-        "input": "-1 0 4 0 3",
+        "input": [
+            "-1 0 4 0 3",
+            5
+        ],
         "output": 4,
     },
     {
-        "input": "9 7 5 5 2 9 9 9 2 -1",
+        "input": [
+            "9 7 5 5 2 9 9 9 2 -1",
+            10
+        ],
         "output": 4,
     },
 ]
@@ -41,7 +54,7 @@ tasks = [
 for task in tasks:
     task_input = task['input']
     task_output = task['output']
-    result = work(task['input'])
+    result = work(task_input[0], task_input[1])
     if task_output != result:
         print("Входное значение:", task_input)
         print("Выходное значение:", task_output)
